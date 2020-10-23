b301bdc8777d808fb17184b7cdc2884c982ef900
Matthew Knight <mattknight.sa@gmail.com>
2020-10-23 12:11:19 -0500

    finishing exercises

--- acquire.py
+++ acquire.py
@@ -1,6 +1,23 @@
 import pandas as pd
+import requests
 
-def new_payload_data():
+def new_payload_items_data():
+    response = requests.get('https://python.zach.lol/api/v1/items')
+    data = response.json()
     df = pd.DataFrame(data['payload']['items'])
-    df.to_csv('new_zillow_df.csv')
+    df.to_csv('new_items_df.csv')
+    return df
+
+def new_payload_stores_data():
+    response = requests.get('https://python.zach.lol/api/v1/items')
+    data = response.json()
+    df = pd.DataFrame(data['payload']['stores'])
+    df.to_csv('new_stores_df.csv')
+    return df
+
+def new_payload_sales_data():
+    response = requests.get('https://python.zach.lol/api/v1/items')
+    data = response.json()
+    df = pd.DataFrame(data['payload']['sales'])
+    df.to_csv('new_sales_df.csv')
     return df
